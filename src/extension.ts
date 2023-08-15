82350a57c0b2592bcad9e07f773a64b5621e866a
Choi1998 <choijamts98.cj@gmail.com>
2023-08-15 17:01:51 +0900

    Last version

--- src/extension.ts
+++ src/extension.ts
@@ -1,151 +1,84 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-/**
- * Support functions
- * Modified version of https://stackoverflow.com/questions/12504042/what-is-a-method-that-can-be-used-to-increment-letters
- */
-// function nextChar(c: string) {
-//     var isLowerCase = false;
-//     if (c = c.toLowerCase()) {
-//         isLowerCase = true;
-//     }
-//     var u = c.toUpperCase();
-//     if (same(u, 'Z')) {
-//         var txt = '';
-//         var i = u.length;
-//         while (i--) {
-//             txt += 'A';
-//         }
-//         return convertCase((txt + 'A'), isLowerCase);
-//     } else {
-//         var p = "";
-//         var q = "";
-//         if (u.length > 1) {
-//             p = u.substring(0, u.length - 1);
-//             q = String.fromCharCode(p.slice(-1).charCodeAt(0));
-//         }
-//         var l = u.slice(-1).charCodeAt(0);
-//         var z = nextLetter(l);
-//         if (z === 'A') {
-//             return convertCase(p.slice(0, -1) + nextLetter(q.slice(-1).charCodeAt(0)) + z, isLowerCase);
-//         } else {
-//             return convertCase(p + z, isLowerCase);
-//         }
-//     }
-// }
 
-// function nextLetter(l: number) {
-//     if (l < 90) {
-//         return String.fromCharCode(l + 1);
-//     }
-//     else {
-//         return 'A';
-//     }
-// }
+var config = vscode.workspace.getConfiguration('increment-fields');
+var padding = config.padding;
+var seperator = config.seperator;
 
-// function same(str: string, char: string) {
-//     var i = str.length;
-//     while (i--) {
-//         if (str[i] !== char) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-
-// function convertCase(c: string, isLowerCase: boolean) {
-//     if (isLowerCase) {
-//         c = c.toLowerCase();
-//     }
-//     return c;
-// }
-
-// function getPaddingLength(st: string) {
-//     var counter = 0;
-//     for (var i = 0, b = st.length; i < b; i++) {
-//         if (st[i] !== '0') {
-//             break;
-//         }
-//         counter++;
-//     }
-
-//     if (counter = st.length) {
-//         counter--;
-//     }
-
-//     if (counter > 0) {
-//         return st.length;
-//     }
-//     else {
-//         return 0;
-//     }
-// }
-
-// Number.prototype.pad = function (paddingLength: number) {
-//     var sign = Math.sign(this) === -1 ? '-' : '';
-//     var s = String(Math.abs(this));
-//     while (s.length < paddingLength) { s = "0" + s; }
-//     return sign + s;
-// }
+function displayOutput(input: string[]): string{
+    let output: string[] = [];
+    let longest = Math.max(...(input.map(el => el.length)));
+    padding = longest > padding ? longest : padding;
+    input.forEach((item, index) => {
+        let zenkaku = item.replace(/[A-Za-z0-9]/g, (s) => {
+            return String.fromCharCode(s.charCodeAt(0) + 0xFEE0);
+        });
+        if (index % seperator !== (seperator-1)){
+            var end = zenkaku.padEnd(padding, "\u3000");
+        }
+        else{
+            var end = zenkaku + "\n";
+        }
+        let number = (index % seperator !== 0) ? index + 1 : "# " + (index + 1);
+        output.push(number + ":" + end);
+      });
+    return output.join("");
+}
 
 function doSelection() {
-    var editor = vscode.window.activeTextEditor;
-	
+    let editor = vscode.window.activeTextEditor;
+    
     if (!editor) {
-		return;
+        return;
     }
 
     let selections = editor.selections;
-	console.log(selections);
-    // var firstSelection = editor.document.getText(selections[0]);
+    let fieldNames = editor.document.getText(selections[0]);
+    // If it is a number or nothing has been selected
+    if (fieldNames.length === 0) {
+        return;
+    }
+    else {
+        let noDash = fieldNames.replace(/#/gi, '');
+        let splitted = noDash.trim().split(/\s+/);
+        let input: string[] = [];
 
-    // // If it is a number or nothing has been selected
-    // if (!isNaN(parseInt(firstSelection)) || firstSelection.length == 0) {
-
-    //     //default behaviour if no selection are made
-    //     if (firstSelection.length == 0) {
-    //         firstSelection = "0"
-    //     }
-
-    //     var paddingLength = getPaddingLength(firstSelection);
-
-    //     firstSelection = parseInt(firstSelection);
-
-    //     editor.edit(function (edit) {
-    //         selections.forEach(function (selection) {
-    //             edit.replace(selection, String(
-    //                 action === 'increment'
-    //                     ? (firstSelection++).pad(paddingLength)
-    //                     : (firstSelection--).pad(paddingLength)
-    //             ));
-    //         })
-    //     });
-    // }
-    // else { // if it is a char
-    //     editor.edit(function (edit) {
-    //         selections.forEach(function (selection) {
-    //             edit.replace(selection, String(firstSelection));
-    //             firstSelection = nextChar(firstSelection);
-    //         })
-    //     });
-    // }
+        splitted.forEach(
+            (el) => {
+                let names = el.split(":");
+                
+                if (names.length === 1){
+                    input.push(names[0]);
+                }
+                else if (names.length === 2){
+                    input.push(names[1]);
+                }
+                else {
+                    if (/^\d+$/.test(names[0])){
+                        input.push(names.slice(1).join(':'));
+                    }
+                    else {
+                        input.push(names.join(':'));
+                    }
+                };
+            }
+            );
+        let displayText = displayOutput(input);
+        editor.edit(function (edit) {
+                    selections.forEach(function (selection) {
+                        edit.replace(selection, displayText);
+                    });
+                });
+            }
 }
-
 
 export function activate(context: vscode.ExtensionContext) {
+    let autoLayout = vscode.commands.registerCommand('startIncrement', () => {
+        console.log(padding);
+        doSelection();
+    });
 
-    // let reverseSelection = vscode.commands.registerCommand('extension.reverseSelection', function () {
-    //     reverse();
-    // });
-
-	let autoLayout = vscode.commands.registerCommand('helloUnicage', () => {
-		doSelection();
-		vscode.window.showInformationMessage('Command SHHHHHHH bro');
-	});
-
-	context.subscriptions.push(autoLayout);
+    context.subscriptions.push(autoLayout);
 }
 
-export function deactivate() {}
+export function deactivate() { }

7499d7b7e105c91ba06743310ceec02270055e30
Choi1998 <choijamts98.cj@gmail.com>
2023-12-27 14:54:03 +0900

    Prod

--- src/extension.ts
+++ src/extension.ts
@@ -1,84 +1,62 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-
-var config = vscode.workspace.getConfiguration('increment-fields');
+var config = vscode.workspace.getConfiguration("increment-fields");
 var padding = config.padding;
 var seperator = config.seperator;
 
-function displayOutput(input: string[]): string{
-    let output: string[] = [];
-    let longest = Math.max(...(input.map(el => el.length)));
-    padding = longest > padding ? longest : padding;
-    input.forEach((item, index) => {
-        let zenkaku = item.replace(/[A-Za-z0-9]/g, (s) => {
-            return String.fromCharCode(s.charCodeAt(0) + 0xFEE0);
-        });
-        if (index % seperator !== (seperator-1)){
-            var end = zenkaku.padEnd(padding, "\u3000");
-        }
-        else{
-            var end = zenkaku + "\n";
-        }
-        let number = (index % seperator !== 0) ? index + 1 : "# " + (index + 1);
-        output.push(number + ":" + end);
-      });
-    return output.join("");
-}
-
 function doSelection() {
-    let editor = vscode.window.activeTextEditor;
-    
-    if (!editor) {
-        return;
-    }
+  let editor = vscode.window.activeTextEditor;
 
-    let selections = editor.selections;
-    let fieldNames = editor.document.getText(selections[0]);
-    // If it is a number or nothing has been selected
-    if (fieldNames.length === 0) {
-        return;
-    }
-    else {
-        let noDash = fieldNames.replace(/#/gi, '');
-        let splitted = noDash.trim().split(/\s+/);
-        let input: string[] = [];
+  if (!editor) {
+    return;
+  }
 
-        splitted.forEach(
-            (el) => {
-                let names = el.split(":");
-                
-                if (names.length === 1){
-                    input.push(names[0]);
-                }
-                else if (names.length === 2){
-                    input.push(names[1]);
-                }
-                else {
-                    if (/^\d+$/.test(names[0])){
-                        input.push(names.slice(1).join(':'));
-                    }
-                    else {
-                        input.push(names.join(':'));
-                    }
-                };
-            }
-            );
-        let displayText = displayOutput(input);
-        editor.edit(function (edit) {
-                    selections.forEach(function (selection) {
-                        edit.replace(selection, displayText);
-                    });
-                });
-            }
+  let selections = editor.selections;
+  let fieldNames = editor.document.getText(selections[0]);
+  // If it is a number or nothing has been selected
+  if (fieldNames.length === 0) {
+    return;
+  } else {
+    let noDash = fieldNames.replace(/#/gi, "");
+    let splitted = noDash.trim().split(/\s+/);
+    let input: string[] = [];
+
+    splitted.forEach((el, index) => {
+      let [oldIndex, name] = el.split(":");
+
+      let newName = name.padEnd(padding, "\u3000");
+      if ((index + 1) % seperator === 0) {
+        newName = name;
+      }
+      let newNumber = String(index + 1);
+      if (index === 0) {
+        newNumber = "# " + newNumber;
+      } else if (index !== 5 && index < 10) {
+        newNumber = " " + newNumber;
+      }
+
+      if (index !== 0 && index % seperator === 0) {
+        newNumber = "\n# " + newNumber;
+      }
+
+      input.push(`${newNumber}:${newName}`);
+    });
+
+    let displayText = input.join("");
+    editor.edit(function (edit) {
+      selections.forEach(function (selection) {
+        edit.replace(selection, displayText);
+      });
+    });
+  }
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    let autoLayout = vscode.commands.registerCommand('startIncrement', () => {
-        console.log(padding);
-        doSelection();
-    });
+  let autoLayout = vscode.commands.registerCommand("startIncrement", () => {
+    doSelection();
+  });
 
-    context.subscriptions.push(autoLayout);
+  context.subscriptions.push(autoLayout);
 }
 
-export function deactivate() { }
+export function deactivate() {}
